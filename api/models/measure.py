8ec534da8908e7e9b8118dd6966513d3d1cf31b4
Adam Sasin <adam.sasin@sprylab.com>
2019-11-12 23:32:43 +0100

    Add mulitlingual setup to admin

--- api/models/measure.py
+++ api/models/measure.py
@@ -6,6 +6,8 @@ from api.models import AbstractModel
 
 class Measure(AbstractModel):
 
+    building_block = models.ForeignKey('BuildingBlock', related_name='measures', on_delete=models.CASCADE, verbose_name=_('building_block'))
+
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True, null=True)
 
